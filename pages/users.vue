1576ff0f1182b4f3e16ab55bb905e3892287be97
shi-zhonghua <shi996966@163.com>
2019-05-13 17:06:00 +0800

    [shizh] 条件选择添加选中状态

--- pages/users.vue
+++ pages/users.vue
@@ -4,6 +4,14 @@
       {{item.label}}
     </li>
     <nuxt-child />
+    <div class="search-condition">
+      <div class="item">
+        <ul v-for="(key,value) in objArr" :key="value">
+            <li>{{value}}:</li>
+            <li v-for="(item,index) in key" @click="getSelect(value,index)" :class="{'active': index===currentIndex[value]}">{{item}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -14,6 +22,15 @@ export default {
   },
   data() {
     return {
+      currentIndex: {
+        bigClass: 0,
+        bigClass1: 0
+      },
+      objArr: {
+        bigClass: ["不限", "sss"],
+        bigClass1: ["不限1", "sss1"],
+      },
+
       userData: [
         { id: 1, label: '手机' },
         { id: 2, label: '电脑' },
@@ -24,8 +41,41 @@ export default {
   methods: {
     gernerateId: function(index) {
       return "person_" + index
+    },
+    getSelect(value, index) {
+      console.log(value);
+      console.log(index);
+      this.currentIndex[value] = index;
     }
   }
 }
 
 </script>
+<style lang="less" scoped>
+.search-condition {
+  .item {
+    box-sizing: border-box;
+    padding-left: 40px;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    border-bottom: solid 1px #ccc;
+    ul{
+      width:100%;
+      overflow: hidden;
+    }
+    li {
+      float: left;
+      font-size: 14px;
+      margin-right: 20px;
+      cursor: pointer;
+      list-style: none;
+
+      &.active {
+        color: #f0f;
+      }
+    }
+  }
+}
+
+</style>
