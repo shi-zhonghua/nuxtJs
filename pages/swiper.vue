6b8bbcdafd4fe2c9b79a08ab371a55a50476eaf7
shi-zhonghua <shi996966@163.com>
2019-08-05 16:28:06 +0800

    [shizh] 第二版swiper 轮播的时候视频暂停播放，ok

--- pages/swiper.vue
+++ pages/swiper.vue
@@ -5,6 +5,9 @@
                 <video id="video" height="260" style="width:100%" src="https://media.w3.org/2010/05/sintel/trailer.mp4" controls="controls" playsinline="" webkit-playsinline="" poster="http://imsapi.yueqizhixiang.com/uploads/images/leqizhixiang/shangjiadianpu/zixun2/1564392065070.jpg">
                     您的浏览器不支持 video 标签。
                 </video>
+                <button @click="pause">暂停</button>
+                <button @click="play">开始</button>
+                <button @click="isPaused">状态</button>
             </div>
             <div class="swiper-slide" v-for="banner in 5">
                 {{banner}}
@@ -29,21 +32,44 @@ export default {
           el: ".swiper-pagination"
           //   dynamicBullets: true // 隐蔽部分分页小点
         },
+
         on: {
-          slideChange() {
-            console.log("onSlideChangeEnd", this);
-          },
-          tap() {
-            console.log("onTap", this);
-          }
+            //  轮播的时候 视频暂停方法
+            slideChangeTransitionEnd: function() {
+	        let videos = document.querySelectorAll('video');
+	        pauseMedia(videos);
+	        // pauseMedia(myAudio, 'audio');
+	        function pauseMedia (media) { // 翻页时媒体暂停播放
+	          for (var i = 0; i < media.length; i++) {
+	            media[i].pause();
+	          }
+	        }
+	      },
         }
       }
     };
   },
+  methods: {
+    pause() {
+      var video = document.getElementById("video");
+      alert("点击暂停");
+      //暂停
+      video.pause();
+    },
+    play() {
+      var video = document.getElementById("video");
+      //播放
+      video.play();
+    },
+    isPaused() {
+      var video = document.getElementById("video");
+      alert(video.paused);
+    }
+  },
   mounted() {
-    console.log("app init", this);
-    console.log(this.mySwiper);
-    // this.mySwiper.slideTo(3);
+    // console.log("app init", this);
+    // console.log(this.mySwiper);
+    // console.log(this.mySwiper);
   }
 };
 </script>
@@ -52,7 +78,7 @@ export default {
   height: 300px;
   width: 300px;
   .swiper-slide {
-   overflow: hidden;
+    overflow: hidden;
   }
   .swiper-pagination {
     > .swiper-pagination-bullet {
